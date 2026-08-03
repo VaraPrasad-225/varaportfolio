53f62c410188cd57c41b35351f42ebdb22e936f4
Vara Prasad <maddurivaraprasad66@gmail.com>
2026-08-03 19:31:11 +0530

    Updated portfolio mobile responsive

--- script.js
+++ script.js
@@ -166,3 +166,9 @@ if (form) {
     });
 
 }
+const menu = document.getElementById("menu-toggle");
+const nav = document.getElementById("nav-links");
+
+menu.addEventListener("click", () => {
+    nav.classList.toggle("active");
+});
